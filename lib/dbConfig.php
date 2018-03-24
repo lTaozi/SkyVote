ea45457ee02ab5a400aaa5b6933c0e0ac6e023a7
skyArony <sky.arony@qq.com>
2018-03-25 02:31:50 +0800

    修改文件

---
+++ lib/dbConfig.php
@@ -0,0 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-type:text/html;charset=utf-8');
+
+
+//Database
+@define('dbHost', 'localhost');
+@define("dbUser", "root");
+// @define("dbPassword", "xxxxxxxxxx");
+@define("dbPassword","xxxxxxxxx");
+// @define("dbPassword","root");
+@define("dbCharset","utf8");
+
